c6870a45ade99f4cd3261ee5b2d228850d972131
m_zandvliet <martijn@ramjetanvil.com>
2018-07-07 12:24:45 +0200

    Ok, so we could take the Tensorflow approach, and first build a CuBLAS-like library of atomic linear algebra operations.

--- Assets/Scripts/Jobs.cs
+++ Assets/Scripts/Jobs.cs
@@ -5,11 +5,69 @@ using UnityEngine;
 public struct AddJob : IJob {
     public NativeArray<float> A;
     public NativeArray<float> B;
-    public NativeArray<float> Result;
+    public NativeArray<float> R;
 
     public void Execute() {
-        for (int i = 0; i < A.Length; i++) {
-            Result[i] = A[i] + B[i];
+        if (A.Length != B.Length || A.Length != R.Length) {
+            Debug.LogError("Arrays need to be of same length.");
+            return;
         }
+
+        for (int i = 0; i < A.Length; i++) {
+            R[i] = A[i] + B[i];
+        }
+    }
+}
+
+public struct SubtractJob : IJob {
+    public NativeArray<float> A;
+    public NativeArray<float> B;
+    public NativeArray<float> R;
+
+    public void Execute() {
+        if (A.Length != B.Length || A.Length != R.Length) {
+            Debug.LogError("Arrays need to be of same length.");
+            return;
+        }
+
+        for (int i = 0; i < A.Length; i++) {
+            R[i] = A[i] - B[i];
+        }
+    }
+}
+
+public struct HadamardJob : IJob {
+    public NativeArray<float> A;
+    public NativeArray<float> B;
+    public NativeArray<float> R;
+
+    public void Execute() {
+        if (A.Length != B.Length || A.Length != R.Length) {
+            Debug.LogError("Arrays need to be of same length.");
+            return;
+        }
+
+        for (int i = 0; i < A.Length; i++) {
+            R[i] = A[i] * B[i];
+        }
+    }
+}
+
+public struct DotJob : IJob {
+    public NativeArray<float> A;
+    public NativeArray<float> B;
+    public float R;
+
+    public void Execute() {
+        if (A.Length != B.Length) {
+            Debug.LogError("Arrays need to be of same length.");
+            return;
+        }
+
+        for (int i = 0; i < A.Length; i++) {
+            R += A[i] * B[i];
+        }
+
+        R /= (float)A.Length;
     }
 }
