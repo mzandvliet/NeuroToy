1dcfd956497d6d41e05d70898cb7024b544747b4
m_zandvliet <martijn@ramjetanvil.com>
2018-07-27 16:13:40 +0200

    1-layer convolution with a random 3x3 kernel on an MNIST image

---
+++ Assets/Scripts/Experiments/Convolution/ConvTest.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using Unity.Collections;
+using Unity.Jobs;
+using DataManager = NNBurst.Mnist.DataManager;
+using NNBurst;
+
+public class ConvTest : MonoBehaviour {
+    private int _imgLabel;
+    private Texture2D _imgTex;
+    private Texture2D _actTex;
+
+    private System.Random _random;
+    
+    
+    private void Awake() {
+        DataManager.Load();
+        _random = new System.Random();
+
+        const int kSize = 3;
+        const int stride = 1;
+
+        var kernel = new NativeArray<float>(kSize*kSize, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+        var img = new NativeArray<float>(28 * 28, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+        var act = new NativeArray<float>(26 * 26, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+
+        const int imgIdx = 0;
+        _imgLabel = DataManager.Train.Labels[imgIdx];
+        var handle = NeuralJobs.CopyInput(img, DataManager.Train, imgIdx);
+
+        NeuralMath.RandomGaussian(_random, kernel, 0f, 1f);
+
+        var cj = new Conv2DJob();
+        cj.input = img;
+        cj.output = act;
+        cj.kernel = kernel;
+        handle = cj.Schedule(handle);
+
+        handle.Complete();
+
+        _imgTex = new Texture2D(28, 28, TextureFormat.ARGB32, false, true);
+        _actTex = new Texture2D(26, 26, TextureFormat.ARGB32, false, true);
+        ToTexture(img, _imgTex);
+        ToTexture(act, _actTex);
+
+        kernel.Dispose();
+        img.Dispose();
+        act.Dispose();
+    }
+
+    private static void ToTexture(NativeArray<float> img, Texture2D tex) {
+        var colors = new Color[img.Length];
+
+        for (int y = 0; y < tex.height; y++) {
+            for (int x = 0; x < tex.width; x++) {
+                float pix = img[y * tex.height + x];
+                // Invert y
+                colors[(tex.height - 1 - y) * tex.height + x] = new Color(pix, pix, pix, 1f);
+            }
+        }
+
+        tex.SetPixels(0, 0, tex.width, tex.height, colors);
+        tex.Apply(false);
+    }
+
+    private void OnGUI() {
+        GUI.Label(new Rect(0f, 32f, 280f, 32f), "Label: " + _imgLabel);
+        GUI.DrawTexture(new Rect(0f, 64f, 280f, 280f), _imgTex, ScaleMode.ScaleToFit);
+        GUI.DrawTexture(new Rect(10f, 64f + 280f, 260f, 260f), _actTex, ScaleMode.ScaleToFit);
+    }
+}
---
+++ Assets/Scripts/NeuralNetwork/Burst/ConvolutionNetwork.cs
@@ -0,0 +1,33 @@
+using Unity.Burst;
+using Unity.Jobs;
+using Unity.Collections;
+
+namespace NNBurst {
+    [BurstCompile]
+    public struct Conv2DJob : IJob {
+        [ReadOnly] public NativeArray<float> input;
+        [ReadOnly] public NativeArray<float> kernel;
+        [WriteOnly] public NativeArray<float> output;
+
+        // Todo: experiment with native slice architectures
+
+        public void Execute() {
+            for (int x = 0; x < 26; x++) {
+                for (int y = 0; y < 26; y++) {
+                    int imgX = x+1;
+                    int imgY = y+1;
+
+                    float act = 0f;
+                    // act = input[imgY * 28 + imgX];
+                    
+                    for (int kX = -1; kX <= 1; kX++) {
+                        for (int kY = -1; kY <= 1; kY++) {
+                            act += input[(imgY+kY) * 28 + (imgX+kX)] * kernel[3 * (1 + kY) + (1 + kX)];
+                        }
+                    }
+                    output[y * 26 + x] = act;
+                }
+            }
+        }
+    }
+}
